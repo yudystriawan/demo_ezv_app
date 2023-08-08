3ec0bcd097593102287d8d6c9a8a69a6512bf9ef
yudystriawan <yudistriawan@gmail.com>
2023-08-08 18:30:25 +0700

    fix UI

--- lib/features/products/presentation/widgets/list_favorite_products.dart
+++ lib/features/products/presentation/widgets/list_favorite_products.dart
@@ -4,7 +4,6 @@ import 'package:demo_ezv_app/features/products/domain/entities/product/product.d
 import 'package:demo_ezv_app/routes/router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 import '../bloc/favorite_actor/product_actor_bloc.dart';
 import '../bloc/loader/product_loader_bloc.dart';
@@ -18,12 +17,14 @@ class ListFavoriteProductWidget extends StatelessWidget {
       buildWhen: (p, c) => p.isLoading != c.isLoading,
       builder: (context, state) {
         final products = state.favoriteProducts;
-        return MasonryGridView.count(
-          crossAxisCount: 2,
+        return ListView.separated(
           itemCount: products.size,
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 10,
-          itemBuilder: (context, index) {
+          separatorBuilder: (BuildContext context, int index) {
+            return const SizedBox(
+              height: 12,
+            );
+          },
+          itemBuilder: (BuildContext context, int index) {
             final product = products[index];
             return ProductItemWidget(product: product);
           },
@@ -43,83 +44,88 @@ class ProductItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        GestureDetector(
-          onTap: () => context.pushRoute(ProductDetailRoute(product: product)),
-          behavior: HitTestBehavior.opaque,
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius:
-                  BorderRadius.circular(15), // Set card's rounded corners
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: Column(
+    return GestureDetector(
+      onTap: () => context.pushRoute(ProductDetailRoute(product: product)),
+      behavior: HitTestBehavior.opaque,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15), // Set card's rounded corners
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
                 children: [
-                  Hero(
-                    tag: product.thumbnailUrl,
-                    child: Image.network(product.thumbnailUrl),
+                  SizedBox(
+                    width: 150,
+                    child: Image.network(
+                      product.thumbnailUrl,
+                    ),
                   ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.all(8),
-                    child: Column(
-                      children: [
-                        Text(
-                          product.title,
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              product.price.toCurrencyFormatted(),
-                              style: TextStyle(
-                                decoration: product.hasDiscount
-                                    ? TextDecoration.lineThrough
-                                    : null,
-                              ),
-                            ),
-                            if (product.hasDiscount) ...[
-                              const SizedBox(
-                                width: 4,
-                              ),
-                              Expanded(
-                                child: Text(
-                                  product.discountedPrice.toCurrencyFormatted(),
-                                ),
-                              ),
-                            ]
-                          ],
-                        ),
-                      ],
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: IconButton(
+                      onPressed: () {
+                        context
+                            .read<ProductActorBloc>()
+                            .add(ProductActorEvent.productRemoved(product.id));
+                      },
+                      icon: const Icon(
+                        Icons.favorite,
+                        color: Colors.amber,
+                      ),
                     ),
                   ),
                 ],
               ),
-            ),
+              const SizedBox(
+                height: 12,
+              ),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        product.title,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            product.price.toCurrencyFormatted(),
+                            style: TextStyle(
+                              decoration: product.hasDiscount
+                                  ? TextDecoration.lineThrough
+                                  : null,
+                            ),
+                          ),
+                          if (product.hasDiscount) ...[
+                            const SizedBox(
+                              width: 4,
+                            ),
+                            Expanded(
+                              child: Text(
+                                product.discountedPrice.toCurrencyFormatted(),
+                              ),
+                            ),
+                          ]
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-        Align(
-          alignment: Alignment.topLeft,
-          child: IconButton(
-            onPressed: () {
-              context
-                  .read<ProductActorBloc>()
-                  .add(ProductActorEvent.productRemoved(product.id));
-            },
-            icon: const Icon(
-              Icons.favorite,
-              color: Colors.amber,
-            ),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
