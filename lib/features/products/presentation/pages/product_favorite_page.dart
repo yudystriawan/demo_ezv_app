6fdb6d38213b5b26c3d5d34915bffc65f03319be
yudystriawan <yudistriawan@gmail.com>
2023-08-08 10:04:57 +0700

    add product pages

---
+++ lib/features/products/presentation/pages/product_favorite_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductFavoritePage extends StatelessWidget {
+  const ProductFavoritePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
