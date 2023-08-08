b79df597816eb17e390c370bafe6f39d40ae7121
yudystriawan <yudistriawan@gmail.com>
2023-08-08 11:24:32 +0700

    add detail page components

--- lib/features/products/presentation/widgets/list_products.dart
+++ lib/features/products/presentation/widgets/list_products.dart
@@ -1,4 +1,6 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:demo_ezv_app/core/helpers/number_formatter.dart';
+import 'package:demo_ezv_app/routes/router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
@@ -29,60 +31,65 @@ class ListProductWidget extends StatelessWidget {
               crossAxisSpacing: 10,
               itemBuilder: (context, index) {
                 final product = products[index];
-                return Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(15), // Set card's rounded corners
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: Column(
-                      children: [
-                        Container(
-                          color: Colors.green,
-                          child: Image.network(product.thumbnailUrl),
-                        ),
-                        const SizedBox(
-                          height: 12,
-                        ),
-                        Container(
-                          width: double.infinity,
-                          padding: const EdgeInsets.all(8),
-                          child: Column(
-                            children: [
-                              Text(
-                                product.title,
-                              ),
-                              const SizedBox(
-                                height: 8,
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    product.price.toCurrencyFormatted(),
-                                    style: TextStyle(
-                                      decoration: product.hasDiscount
-                                          ? TextDecoration.lineThrough
-                                          : null,
-                                    ),
-                                  ),
-                                  if (product.hasDiscount) ...[
-                                    const SizedBox(
-                                      width: 4,
-                                    ),
-                                    Expanded(
-                                      child: Text(
-                                        product.discountedPrice
-                                            .toCurrencyFormatted(),
+                return GestureDetector(
+                  onTap: () =>
+                      context.pushRoute(ProductDetailRoute(product: product)),
+                  behavior: HitTestBehavior.opaque,
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          15), // Set card's rounded corners
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Column(
+                        children: [
+                          Hero(
+                            tag: product.thumbnailUrl,
+                            child: Image.network(product.thumbnailUrl),
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.all(8),
+                            child: Column(
+                              children: [
+                                Text(
+                                  product.title,
+                                ),
+                                const SizedBox(
+                                  height: 8,
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      product.price.toCurrencyFormatted(),
+                                      style: TextStyle(
+                                        decoration: product.hasDiscount
+                                            ? TextDecoration.lineThrough
+                                            : null,
                                       ),
                                     ),
-                                  ]
-                                ],
-                              ),
-                            ],
+                                    if (product.hasDiscount) ...[
+                                      const SizedBox(
+                                        width: 4,
+                                      ),
+                                      Expanded(
+                                        child: Text(
+                                          product.discountedPrice
+                                              .toCurrencyFormatted(),
+                                        ),
+                                      ),
+                                    ]
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 );
